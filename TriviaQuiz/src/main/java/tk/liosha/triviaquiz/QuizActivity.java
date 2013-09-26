bc83f93012338bd8346528f46cada57a76634b3a
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-09-26 11:00:23 +0300

    Initial commit: Created empty project

---
+++ TriviaQuiz/src/main/java/tk/liosha/triviaquiz/QuizActivity.java
@@ -0,0 +1,23 @@
+package tk.liosha.triviaquiz;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class QuizActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz_layout);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+    
+}
